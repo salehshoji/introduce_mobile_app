6b46fe2a6d88122bc3eac5fdb8cd2611d2bc6255
salehshoji <mohamadsaleh.shojai@yahoo.com>
2023-05-22 16:08:23 +0330

    long name support

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -12,6 +14,11 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         ImageView image = (ImageView) findViewById(R.id.image_view);
         image.setClipToOutline(true);
+        TextView tv = (TextView) findViewById(R.id.name_TV);
+        if (tv.getText().length() > 17){
+            tv.setText(tv.getText().subSequence(0, 15) + "...");
+        }
+
     }
 
 }
