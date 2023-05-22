9dc25c700dc41d336f714deac08816382db916a2
salehshoji <mohamadsaleh.shojai@yahoo.com>
2023-05-22 18:49:08 +0330

    finish email button

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.myapplication;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.Manifest;
@@ -21,12 +20,17 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(), "welcome to saleh app", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "welcome to saleh app", Toast.LENGTH_LONG).show();
+
         ImageView image = (ImageView) findViewById(R.id.image_view);
         image.setClipToOutline(true);
-        TextView tv = (TextView) findViewById(R.id.name_TV);
+        TextView tv = (TextView) findViewById(R.id.name_textview);
         if (tv.getText().length() > 17){
             tv.setText(tv.getText().subSequence(0, 15) + "...");
         }
+
+        // call button
         Button call_button = (Button) findViewById(R.id.call_button);
         call_button.setOnClickListener(arg -> {
             checkPermission(Manifest.permission.CALL_PHONE, 1);
@@ -41,6 +45,19 @@ public class MainActivity extends Activity {
             // start Intent
             startActivity(phone_intent);
         });
+
+        // email button
+        Button email_button = (Button) findViewById(R.id.email_button);
+        email_button.setOnClickListener(view -> {
+
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("plain/text");
+            intent.putExtra(Intent.EXTRA_EMAIL, new String[] { "mohamadsaleh.shojai@gmail.com" });
+            startActivity(Intent.createChooser(intent, "to app owner"));
+            // define Intent object with action attribute as ACTION_SEND
+        });
+
+
     }
 
 
